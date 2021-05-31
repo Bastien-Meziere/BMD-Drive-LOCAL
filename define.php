cbb6057803f1e5a330d06805bfe3721b58d7d7e8
Bastien <bastien.meziere@protonmail.com>
2021-05-31 14:47:08 +0200

    Remplacement chemin BMDDRIVEOfficiel par BMD-Drive-LOCAL

--- define.php
+++ define.php
@@ -1,9 +1,9 @@
 <?php 
 define('WEBROOT',str_replace('index.php','',$_SERVER['SCRIPT_NAME']));
 define('ROOT',str_replace('index.php','',$_SERVER['SCRIPT_FILENAME']));
-define('IMAGE','/BMDDRIVEOfficiel/asset/');
-define('SCRIPT','/BMDDRIVEOfficiel/asset/js/');
-define('SHEET','/BMDDRIVEOfficiel/asset/')
+define('IMAGE','/BMD-Drive-LOCAL/asset/');
+define('SCRIPT','/BMD-Drive-LOCAL/asset/js/');
+define('SHEET','/BMD-Drive-LOCAL/asset/')
 
 //$GLOBAL['pdo']=$pdo->exec('SET NAMES utf8');
 
